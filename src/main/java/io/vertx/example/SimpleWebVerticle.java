fda3ace8c55c036245acec560ee8571b58b52d45
Claus Ibsen <claus.ibsen@gmail.com>
2016-11-10 13:41:21 +0100

    Use expose instead of LB

--- src/main/java/io/vertx/example/SimpleWebVerticle.java
+++ src/main/java/io/vertx/example/SimpleWebVerticle.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2005-2015 Red Hat, Inc.
+ * <p>
+ * Red Hat licenses this file to you under the Apache License, version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 package io.vertx.example;
 
 import io.vertx.core.AbstractVerticle;
@@ -8,24 +23,24 @@ import io.vertx.ext.web.RoutingContext;
 
 public class SimpleWebVerticle extends AbstractVerticle {
 
-  @Override
-  public void start(Future<Void> startFuture) throws Exception {
-    Router router = Router.router(vertx);
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+        Router router = Router.router(vertx);
 
-    router.get("/").handler(this::handleGet);
+        router.get("/").handler(this::handleGet);
 
-    vertx.createHttpServer().requestHandler(router::accept).listen(8080, ar -> {
-      if (ar.succeeded()) {
-        startFuture.complete();
-      } else {
-        startFuture.fail(ar.cause());
-      }
-    });
-  }
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080, ar -> {
+            if (ar.succeeded()) {
+                startFuture.complete();
+            } else {
+                startFuture.fail(ar.cause());
+            }
+        });
+    }
 
-  private void handleGet(RoutingContext routingContext) {
-    HttpServerResponse response = routingContext.response();
-    String welcome = routingContext.request().getParam("welcome");
-    response.end("Reply: " + welcome);
-  }
+    private void handleGet(RoutingContext routingContext) {
+        HttpServerResponse response = routingContext.response();
+        String welcome = routingContext.request().getParam("welcome");
+        response.end("Reply: " + welcome);
+    }
 }
